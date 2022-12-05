081ab5ef11e9076e93ea39f2c93f4af7b07c81ae
Michael Vel <msvelandiag@unal.edu.co>
2022-12-05 16:55:09 -0500

    day 4

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,18 @@ use std::io::prelude::*;
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
+pub mod day5;
 
 fn main() -> std::io::Result<()> {
-    let mut file = File::open("./data/day3.txt")?;
+    let mut file = File::open("./data/day4.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let result = day3::solution1(&contents);
+    let result = day4::solution1(&contents);
     println!("The result is: {result}");
 
-    let result2 = day3::solution2(&contents);
+    let result2 = day4::solution2(&contents);
     println!("The second part result is: {result2}");
     Ok(())
 }
